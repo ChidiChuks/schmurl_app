9d076e5753b191d6e21648330e9fcb5003b656d7
ChidiChuks <chukschiboy@gmail.com>
2020-04-07 18:52:39 +0100

    creating a container wrapped around on the questionaire

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 
+// personal imports
+import 'package:schmurl_app/question.dart';
+
 // void main() {
 //   runApp(MyApp());
 // }
@@ -11,16 +14,19 @@ class MyApp extends StatefulWidget {
   @override
   State<StatefulWidget> createState() {
     // TODO: implement createState
-    return MyAppState();
+    return _MyAppState();
   }
 }
 
-class MyAppState extends State<MyApp> {
-  var questionIndex = 0;
+class _MyAppState extends State<MyApp> {
+  var _questionIndex = 0;
 
-  void answerQuestion() {
-    questionIndex = questionIndex + 1;
-    print(questionIndex);
+  void _answerQuestion() {
+    setState(() {
+      _questionIndex = _questionIndex + 1;
+    });
+      
+    print(_questionIndex);
   }
 
   @override
@@ -36,12 +42,12 @@ class MyAppState extends State<MyApp> {
         ),
         body: new Column(
           children: <Widget>[
-            Text(
-              questions[questionIndex],
+            Question(
+              questions[_questionIndex],
             ),
             RaisedButton(
               child: Text('Answer 1'),
-              onPressed: answerQuestion,
+              onPressed: _answerQuestion,
             ),
             RaisedButton(
               child: Text('Answer 2'),
