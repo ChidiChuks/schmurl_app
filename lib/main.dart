26193ae95c1ea2bed06333ed55386188ca49d11d
ChidiChuks <chukschiboy@gmail.com>
2020-04-08 00:41:48 +0100

    a fully covered concept on stateful and stateless widget application

--- lib/main.dart
+++ lib/main.dart
@@ -24,24 +24,49 @@ class _MyAppState extends State<MyApp> {
   final _questions = const [
     {
       'questionText': 'What\'s your favorite color?', 
-      'answers': ['Black', 'Red', 'Green', 'White'],
+      'answers': [
+        {'text': 'Black', 'score': 10}, 
+        {'text': 'Red', 'score': 5}, 
+        {'text': 'Green', 'score': 3}, 
+        {'text': 'White', 'score': 1}
+      ],
     },
     {
       'questionText': 'What\'s your favorite animal?',
-      'answers': ['Cat', 'Lion', 'Eagle', 'Snake']
+      'answers': [
+        {'text': 'Cat', 'score': 10}, 
+        {'text': 'Lion', 'score': 3}, 
+        {'text': 'Eagle', 'score': 5}, 
+        {'text': 'Snake', 'score': 1}
+      ],
     },
     {
       'questionText': 'Who\'s your favorite instructor?',
-      'answers': ['Max', 'Chinedu', 'Pat', 'John']
+      'answers': [
+        {'text': 'Max', 'score': 10}, 
+        {'text': 'Chinedu', 'score': 3}, 
+        {'text': 'Pat', 'score': 5}, 
+        {'text': 'John', 'score': 1}
+      ],
     },
   ];
 
   var _questionIndex = 0;
+  var _totalScore = 0;
 
-  void _answerQuestion() {
+  void _resetQuiz() {
+    setState(() {
+      _questionIndex = 0;
+      _totalScore = 0;
+    });
+  }
+
+  void _answerQuestion(int score) {
 
     // var aBool = true;
     // aBool = false;
+
+    _totalScore += score;
      
     setState(() {
       _questionIndex = _questionIndex + 1;
@@ -66,7 +91,7 @@ class _MyAppState extends State<MyApp> {
         ),
         body: _questionIndex < _questions.length 
         ? Quiz(answerQuestion: _answerQuestion, questionIndex: _questionIndex, questions: _questions,) : 
-        Result(),
+        Result(_totalScore, _resetQuiz),
       ),
     );
   }
